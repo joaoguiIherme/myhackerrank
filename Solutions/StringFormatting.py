b6cdf5af0cfabd1a958556f8cee8cc5b06512f3f
Joao Guilherme <jgwaquim@hotmail.com>
2020-09-04 10:18:00 -0300

    Add String Formatting challenge solution

---
+++ Solutions/StringFormatting.py
@@ -0,0 +1,6 @@
+def print_formatted(number):
+    for i in range(1,number+1):
+        l=len('{0:b}'.format(number))
+        l=int(l)
+        print('{0:{width}d} {0:{width}o} {0:{width}X} {0:{width}b}'.format(i,width=l))
+        
